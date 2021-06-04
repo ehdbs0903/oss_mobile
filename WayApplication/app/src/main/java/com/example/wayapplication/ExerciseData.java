2b401849b54e44f924e45f02a7612be4a9c9177d
Hyeonju Seo <preengus@gmail.com>
2021-06-04 13:38:06 +0900

    add exerciseWay

---
+++ WayApplication/app/src/main/java/com/example/wayapplication/ExerciseData.java
@@ -0,0 +1,51 @@
+package com.example.wayapplication;
+
+public class ExerciseData {
+    private int image;
+    private String name;
+    private String bodyPart;
+    private String wayExercise;
+    private String reps;
+    private String extraExercise;
+    private boolean expanded;
+
+    public ExerciseData(int image, String name, String bodyPart, String wayExercise, String reps, String extraExercise) {
+        this.image = image;
+        this.name = name;
+        this.bodyPart = bodyPart;
+        this.wayExercise = wayExercise;
+        this.reps = reps;
+        this.extraExercise = extraExercise;
+    }
+
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBodyPart() {
+        return bodyPart;
+    }
+
+    public String getWayExercise() { return wayExercise; }
+
+    public String getReps() {
+        return reps;
+    }
+
+    public String getExtraExercise() {
+        return extraExercise;
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+}
